c61006aaa66d75bdb9aa8527c7b738363fb459db
Diego Di Giorgio <diegofdg@gmail.com>
2021-09-13 20:54:50 -0300

    creating the front-end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from './components/Form';
+import Section from './components/Section';
+import List from './components/List';
+
+const appTitle = 'To-Do List App';
+
+const list = [ 
+    { id: 1, title: "Test #1", completed: false },
+    { id: 2, title: "Test #2", completed: false },
+    { id: 3, title: "Test #3", completed: false }
+];
 
 function App() {
-  return (
-    <h1>To-Do List App</h1>
-  );
-}
+    const [todoList, setTodoList] = useState(list);
+
+    const addTodo = (item) => {        
+        setTodoList((oldList) => [...oldList, item]);
+    };
+
+    const removeTodo = (id) => {
+        setTodoList((oldList) => oldList.filter((item) => item.id !== id));  
+    };
+
+    return (
+        <div className='ui container center aligned'>
+            <Section>
+                <h1>{appTitle}</h1>
+            </Section>
+          
+            <Section>
+                <Form addTodo={addTodo}/>
+            </Section>
+
+            <Section>
+                <List removeTodoListProp={removeTodo} list={todoList} />
+            </Section>
+        </div>
+    );
+};
 
 export default App;
